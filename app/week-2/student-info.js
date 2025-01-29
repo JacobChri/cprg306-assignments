7b707841b4374473a91bb003006ec1955ccbbd00
JacobChri <jacobchri75@gmail.com>
2025-01-28 20:30:12 -0700

    Week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function StudentInfo () {
+    return (
+        <main>
+            <p>Jacob Christensen</p>
+            <Link href='http://github.com/JacobChri/cprg306-assignments'>
+            http://github.com/JacobChri/cprg306-assignments
+            </Link>
+        </main>
+    )
+}
